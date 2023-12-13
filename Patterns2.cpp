919eaac26ab3a6b5373899d3dab13324600fd7db
Karishma Moond <93302936+Karishma-Moond@users.noreply.github.com>
2023-12-13 15:23:32 +0530

    Create Patterns2.cpp

---
+++ Patterns2.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+cout<<"Pattern 11: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+    cout<<"Pattern 12: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+    cout<<"Pattern 13: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+    cout<<"Pattern 14: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+    cout<<"Pattern 15: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+    cout<<"Pattern 16: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+    cout<<"Pattern 17: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+    cout<<"Pattern 18: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+    cout<<"Pattern 19: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+    cout<<"Pattern 20: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+}
