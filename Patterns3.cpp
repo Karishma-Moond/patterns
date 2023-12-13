df4eaadaae9261d1a1ac684c0347fdf620e70567
Karishma Moond <93302936+Karishma-Moond@users.noreply.github.com>
2023-12-13 15:23:40 +0530

    Create Patterns3.cpp

---
+++ Patterns3.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+cout<<"Pattern 21: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+    cout<<"Pattern 22: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+    cout<<"Pattern 23: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+    cout<<"Pattern 24: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+    cout<<"Pattern 25: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+    cout<<"Pattern 26: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+
+    cout<<"Pattern 27: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+    cout<<"Pattern 28: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+    cout<<"Pattern 29: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+    cout<<"Pattern 30: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/}
