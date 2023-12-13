a30dd98d16fa67b03cb35c87d4886247f849d821
Karishma Moond <93302936+Karishma-Moond@users.noreply.github.com>
2023-12-13 15:23:19 +0530

    Create Patterns1.cpp

---
+++ Patterns1.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+  cout<<"Pattern 1: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+  for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+        cout<<"*"<<" ";
+    } cout<<endl;
+
+
+    cout<<"Pattern 2: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+    cout<<"Pattern 3: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+    cout<<"Pattern 4: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+    cout<<"Pattern 5: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+    cout<<"Pattern 6: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+    cout<<"Pattern 7: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+    cout<<"Pattern 8: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+    cout<<"Pattern 9: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+    cout<<"Pattern 10: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+  }
+    
+    cout<<"Pattern 1: \n";
+  /*  n=3
+      ***
+      ***
+      ***
+*/
+
+  
+}
