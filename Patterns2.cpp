8b182591b4239211dd1fae3c56aa0436baf1f97e
Karishma Moond <93302936+Karishma-Moond@users.noreply.github.com>
2023-12-13 16:49:53 +0530

    Patterns2.cpp

--- Patterns2.cpp
+++ Patterns2.cpp
@@ -4,23 +4,72 @@ int main(){
 int n;
 cin>>n;
 cout<<"Pattern 11: \n";
-  /*  n=3
-      ***
-      ***
-      ***
+  /*  n=5
+     *****
+      **** 
+       ***  
+        **   
+         * 
 */
+  for(int i=0;i<n;i++){
+    //space
+    for(int j=1;j<=i;j++){
+        cout<<" ";
+    }
+    //star
+    for(int j=1;j<=n-i;j++){
+        cout<<"*";
+    }
+    //space
+    for(int j=1;j<=i;j++){
+        cout<<" ";
+    }
+    cout<<endl;
+}
+
+
+  
     cout<<"Pattern 12: \n";
   /*  n=3
-      ***
-      ***
-      ***
+*
+**
+***
+**
+*
 */
+   for(int i=1;i<=2*n-1;i++){
+        int star=i;
+        if (i>n) star=2*n-i;
+        for(int j=1;j<=star;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+	}
+
+  
     cout<<"Pattern 13: \n";
-  /*  n=3
-      ***
-      ***
-      ***
+  /*  n=4
+1
+0 1
+1 0 1
+0 1 0 1
 */
+   int start=1;
+    for(int i=0;i<n;i++){
+        if(i%2==0){
+            start=1;
+        }else {
+            start=0;
+        }
+        for(int j=0;j<=i;j++){
+            cout<<start;
+            start=1-start;
+        }
+        cout<<endl;
+    }
+
+
+  
     cout<<"Pattern 14: \n";
   /*  n=3
       ***
